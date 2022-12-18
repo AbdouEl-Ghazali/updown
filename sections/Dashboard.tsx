9be79fe5905bf3292fb456efadef28208f41f5d9
Abdou <107400373+AbdouEl-Ghazali@users.noreply.github.com>
2022-12-18 12:43:07 -0800

    attempt 3: using main folder as root

--- sections/Dashboard.tsx
+++ sections/Dashboard.tsx
@@ -7,7 +7,7 @@ const Dashboard = () => {
   return (
     <div className='flex justify-between place-items-center px-5 py-1 bg-zinc-100 dark:bg-zinc-800'>
       <div className='flex flex-row justify-center place-items-center gap-1'>
-        <img style={{ width: 50, height: 50 }} src={'./updown/components/images/logoSVG.svg'} alt="UpDown Logo" />
+        <img style={{ width: 50, height: 50 }} src={'./components/images/logoSVG.svg'} alt="UpDown Logo" />
         <h1>
           UpDown
         </h1>
