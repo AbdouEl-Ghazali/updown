f40ac9a61b056c98775eb746d858a7fc90a3d0c0
Abdou <107400373+AbdouEl-Ghazali@users.noreply.github.com>
2022-12-18 13:35:43 -0800

    changed SVG source

--- sections/Dashboard.tsx
+++ sections/Dashboard.tsx
@@ -2,13 +2,14 @@ import React from 'react'
 import Darkmode from '../components/Darkmode'
 import styles from '../styles/Home.module.css'
 import Image from 'next/image'
+import logoSVG from '../public/logoSVG.svg'
 
 const Dashboard = () => {
 
   return (
     <div className='flex justify-between place-items-center px-5 py-1 bg-zinc-100 dark:bg-zinc-800'>
       <div className='flex flex-row justify-center place-items-center gap-1'>
-        <Image src={'/logoSVG.svg'} alt="UpDown Logo" width={50} height={50} />
+        <Image src={logoSVG} alt="UpDown Logo" width={50} height={50} />
         <h1>
           UpDown
         </h1>
